8b1a5b5647febba9ffcf1f219c19d98311dc4084
mishradibyesh <dibyeshmishra207@gmail.com>
2021-10-02 22:43:43 +0530

    Add part time employee and wage

--- EmpWageComputation.java
+++ EmpWageComputation.java
@@ -1,23 +1,31 @@
 import java.util.*;
 import java.lang.*;
 public class EmpWageComputation {
-	public static void main(String[] args) {
+static final int Fulltime=1;
+static final int  Parttime=2;
+        public static void main(String[] args) {
                         System.out.println("Welcome to EMPLOYEE WAGE COMPUTAION PROGRAM");
-                        int Is_Present=(int)Math.floor(Math.random()*10)%2; //creating random
+                        int Random=(int)Math.floor(Math.random()*10)%3; //creating random
                         int salary=0;
-                        if(Is_Present==1)   // checking presence with the help of if condition
+                        if(Random==Fulltime)   // checking presence with the help of if condition
                         {	
                     	     int FullDayHour=8;
                              int WagePerHour=20;
                     	     salary+=WagePerHour*FullDayHour;
-                             System.out.println("Employee is present and his today's income is Rs. "+salary);
-                        
+                             System.out.println("Employee is full time and his today's income is Rs. "+salary);
                         }
-                        else
-                   	     System.out.println("Employee is absent and his today's income is Rs. "+salary);
-}
+                        // checking if employee is part time
+                        else if(Random==Parttime) {
+                              int FullDayHour1=4;
+                              int WagePerHour1=20;
+                       	      salary+=WagePerHour1*FullDayHour1; 
+                              System.out.println("Employee is part time and his today's income is Rs. "+salary);
+
+                         }
+                        
+                         else if (Random==0) { 
+                              System.out.println("Employee is absent and his today's income is Rs. 0");
+                         }
+                }
         }
-
-
-
 
