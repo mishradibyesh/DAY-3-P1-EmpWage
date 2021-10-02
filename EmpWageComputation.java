7ae9f21683c0eca205bc7f2cb9b7278a5b56f421
mishradibyesh <dibyeshmishra207@gmail.com>
2021-10-02 22:56:37 +0530

    calculating wages for a month

--- EmpWageComputation.java
+++ EmpWageComputation.java
@@ -1,10 +1,12 @@
 import java.util.*;
 import java.lang.*;
-public class UC_4 {
+public class EmpWageComputation {
 static final int Fulltime=1;
 static final int  Parttime=2;
         public static void main(String[] args) {
                         System.out.println("Welcome to EMPLOYEE WAGE COMPUTAION PROGRAM");
+                        int TotalMonthlySalary=0;
+                        for(int i=1; i<=20;i++) {
                         int Random=(int)Math.floor(Math.random()*10)%3; //creating random
                         int salary=0;
                         switch (Random)   // checking presence with the help of if condition
@@ -14,17 +16,22 @@ static final int  Parttime=2;
                     	     int FullDayHour=8;
                              int WagePerHour=20;
                     	     salary+=WagePerHour*FullDayHour;
-                             System.out.println("Employee is full time and his today's income is Rs. "+salary);
+                             System.out.println("Employee is full time and his day "+i+" income is Rs. "+salary);
                              break;
                         case Parttime:
                              int FullDayHour1=4;
                              int WagePerHour1=20;
                              salary+=WagePerHour1*FullDayHour1; 
-                             System.out.println("Employee is part time and his today's income is Rs. "+salary);
+                             System.out.println("Employee is part time and his day "+i+" income is Rs. "+salary);
                              break;
  
                         default : 
-                   	     System.out.println("Employee is absent and his today's income is Rs. 0");
-                        }                      
-                }
-        }
+                   	      System.out.println("Employee is absent and his day "+i+" income is Rs. 0");
+                        } 
+                             //  calculating the total monthly income
+                              TotalMonthlySalary+=salary;
+                        }
+                              System.out.println("Employee's monthly salary is Rs. "+TotalMonthlySalary);
+   
+           }
+       }
