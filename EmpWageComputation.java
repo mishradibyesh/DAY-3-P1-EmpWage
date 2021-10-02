a8d6c94070a1a5b1da284f8619c098c6fc400f8c
mishradibyesh <dibyeshmishra207@gmail.com>
2021-10-02 21:43:16 +0530

    Start with Welcome message

---
+++ EmpWageComputation.java
@@ -0,0 +1,7 @@
+import java.util.*;
+import java.lang.*;
+public class EmpWageComputation {
+	public static void main(String[] args) {	
+			System.out.println("Welcome to EMPLOYEE WAGE COMPUTAION PROGRAM");
+}
+	}
