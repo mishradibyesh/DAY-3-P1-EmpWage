0c45d1876c61d7e6a9ae477e0970790619b0ae8e
mishradibyesh <dibyeshmishra207@gmail.com>
2021-10-02 22:26:45 +0530

    checking employee is absent or present

--- EmpWageComputation.java
+++ EmpWageComputation.java
@@ -2,6 +2,15 @@ import java.util.*;
 import java.lang.*;
 public class EmpWageComputation {
 	public static void main(String[] args) {	
-			System.out.println("Welcome to EMPLOYEE WAGE COMPUTAION PROGRAM");
-}
-	}
+	        	System.out.println("Welcome to EMPLOYEE WAGE COMPUTAION PROGRAM");
+                        int Is_Present=(int)Math.floor(Math.random()*10)%2; //creating random number
+                        if(Is_Present==1)   // checking presence with the help of if condition
+    	                    System.out.println("Employee is present");
+                        else
+    	                    System.out.println("Employee is absent"); 
+                         }
+	            }
+
+
+
+
